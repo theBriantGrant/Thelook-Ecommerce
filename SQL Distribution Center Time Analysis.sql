36a93ce66d0dfb9b166500cbe94270f56c4b8b5e
theBriantGrant <154294499+theBriantGrant@users.noreply.github.com>
2026-03-23 12:13:27 -0400

    Update SQL Distribution Center Time Analysis.sql

--- SQL Distribution Center Time Analysis.sql
+++ SQL Distribution Center Time Analysis.sql
@@ -1,10 +1,13 @@
+--First, create a CTE allows the avg of the inventory days in the main body
 WITH avg_inv_calc AS (
 SELECT d.name, ROUND(AVG(TIMESTAMP_DIFF(ii.sold_at, ii.created_at, DAY)),2)  AS avg_inv_storage_days
+--Using TIMESTAMP_DIFF to see how long an item was in the inventory before it was sold
 FROM `bigquery-public-data.thelook_ecommerce.inventory_items` AS ii
 JOIN `bigquery-public-data.thelook_ecommerce.distribution_centers` AS d
   ON ii.product_distribution_center_id = d.id
 GROUP BY d.name
 ),
+--Create a CTE that sums the total price of order items with a return data
   return_price_calc AS (
     SELECT d.name, ROUND(SUM(oi.sale_price),2) AS return_price_sum
 FROM `bigquery-public-data.thelook_ecommerce.distribution_centers` AS d
@@ -15,6 +18,7 @@ LEFT JOIN `bigquery-public-data.thelook_ecommerce.order_items` AS oi
 WHERE oi.returned_at IS NOT NULL
 GROUP BY d.name
 )
+--Select the desired differences in timestamps and join required tables
 SELECT  
 d.name AS distib_center,
 ROUND(AVG(TIMESTAMP_DIFF(o.shipped_at, o.created_at, HOUR)),2) AS avg_order_ship_hr,
